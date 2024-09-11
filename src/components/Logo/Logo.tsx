2b00e5bc4a8caa07920d2a9173c58689666fa4bf
Damola <odamola@mit.edu>
2024-09-11 21:02:01 +0100

    updated Logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -5,10 +5,10 @@ import styles from './Logo.module.css';
 const Logo: FunctionComponent = () => {
   return (
     <div className={styles.logo}>
-      <img className={styles.chipIcon} alt="Chip" src="Chip.svg" />
+      <img className={styles.chipIcon} alt="Chip" src="../Assets/images/Chip.svg" />
       <div className={styles.whatAMarket}>What a Market!</div>
     </div>
   );
 };
 
-export default Logo;
+export default Logo;git sts
